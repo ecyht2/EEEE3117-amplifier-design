38a1f04c8ebb0518a4eaa80d149a3f772c2782a2
ecyht2 <94816144+ecyht2@users.noreply.github.com>
2024-04-20 10:07:42 +0000

    feat: Added more value printing

--- scripts/run.py
+++ scripts/run.py
@@ -55,7 +55,20 @@ if __name__ == "__main__":
     M3 = (1e3, 6.5e3, 80e3)
     amp = Amplifier(*Q1, *Q2, *M3)
 
+    print("Transistor Current")
+    print(f"Q1: I_B1: {amp.I_B1:.3g}, I_C1: {amp.I_C1:.3g}, I_E1: {amp.I_E1:.3g}")
+    print(f"Q2: I_B2: {amp.I_B2:.3g}, I_C2: {amp.I_C2:.3g}, I_E2: {amp.I_E2:.3g}")
+    print(f"M3: I_D: {amp.I_D:.3g}")
+
+    print("Transistor Validity")
     print(f"Q1: {amp.Q1_valid()}, V_C: {amp.V_C1}, V_B: {amp.V_B1}, V_E: {amp.V_E1}")
     print(f"Q2: {amp.Q2_valid()}, V_C: {amp.V_C2}, V_B: {amp.V_B2}, V_E: {amp.V_E2}")
     print(f"M3: {amp.M3_valid()}, V_D: {amp.V_D}, V_G: {amp.V_G}, V_S: {amp.V_S}")
+
+    print("AC Values")
+    print(f"Q1: g_m: {amp.g_m1:.3f}, r_0: {amp.r_01:.3f}, r_pi: {amp.r_pi1:.3f}")
+    print(f"Q2: g_m: {amp.g_m2:.3f}, r_0: {amp.r_02:.3f}, r_pi: {amp.r_pi2:.3f}")
+    print(f"M3: g_m: {amp.g_m3:.3f}, r_0: {amp.r_03:.3f}")
+
+    print("AC Analysis")
     print(f"A_v: {amp.gain}")
